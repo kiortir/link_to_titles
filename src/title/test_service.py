fa1463d9c3c0b647bd1814973340b1496b86c412
Michael <mpgg2000@gmail.com>
2023-02-02 22:01:29 +0300

    Revert "Merge pull request #1 from kiortir/iterations"
    This reverts commit 837a73862f286fbdc3143124900608751e96ce44, reversing
    changes made to 18a2dba87cdfc4c10cd85e339fc37bfa835947c4.

--- src/title/test_service.py
+++
@@ -1,9 +0,0 @@
-from title.service import get_title
-
-def test_title_extractor() -> None:
-
-    titles = [
-        '<title>MyTitle</title>',
-        '<title randomattr="true">MyTitle</title>',
-    ]
-    assert all([get_title(title) == 'MyTitle' for title in titles])
